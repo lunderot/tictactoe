d761d8b6d8b71ea73066dc10e4f0f380e4102c32
lunderot <mattias.lunderot@gmail.com>
2019-06-11 21:38:40 +0200

    Added peerjs

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+import Peer from 'peerjs';
+var peer = new Peer();
+
+if (window.location.hash) {
+	console.log('connecting to ' + window.location.hash.substring(1));
+	const conn = peer.connect(window.location.hash.substring(1));
+	conn.on('open', (id) => {
+		conn.on('data', function (data) {
+			console.log('Received', data);
+		});
+		conn.send('hi!');
+	});
+}
+else {
+	peer.on('connection', (conn) => {
+		conn.on('data', (data) => {
+			console.log(data);
+		});
+	});
+	peer.on('open', (id) => {
+		document.write('<a target="_blank" rel="noopener noreferrer" href="' + 'http://localhost:1234#' + id + '">Click to open!</a>');
+	});
+}
